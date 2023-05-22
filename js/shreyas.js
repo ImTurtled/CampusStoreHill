f3fef1bcaf70fafcc11149b19406afcdb8cd3143
Shreyask2 <korithiwadas@gmail.com>
2023-05-21 21:12:46 -0400

    a

---
+++ js/shreyas.js
@@ -0,0 +1,5 @@
+function goToProductPage(productId) {
+  // You can handle the navigation to the product page here
+  // For demonstration purposes, let's simply display an alert with the selected product ID
+  alert("You clicked on Product " + productId);
+}
